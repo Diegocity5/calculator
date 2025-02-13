325bfc9742ca2b6e2847992ae75c7a0413d1400d
Diegocity5 <icotecc@gmail.com>
2025-02-13 02:27:51 -0500

    Limpiar la primera vez antes de comenzar a escribir

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,29 @@
 //Selecionando la pantalla
 const displayCalc = document.querySelector('#calc-result');
 const keyboard = document.querySelector('#keyboard-container');
-let countFirstClick = 0;
-let message = 'Colombia';
-
-//validar que la expresion cumpla con ciertos requisitos para ser operado.
-const regex = /[0-9]+\s*[+\-*/]\s*[0-9]+/;
-
-//validacion que para verificar si va teclando correctamente una expresion numerica.
-// console.log(regex.test(expresionN));
-
+let countFirstEvent = 0;
 
 //Detectar eventos de las teclas
 keyboard.addEventListener('click', function(e){
     const targetClassName = e.target.className;
-    countFirstClick++;
+    countFirstEvent++;
 
-    if(countFirstClick === 1){
-        clearDisplay();
-        if(targetClassName === 'key-operator' || targetClassName === 'key-number'){
-            if(e.target.textContent === 'ce'){
-                clearDisplay();
-            }
-            //Cambiando dinamicamente la información del la pantalla
-            displayCalc.textContent += e.target.textContent;
+    if(targetClassName === 'key-operator' || targetClassName === 'key-number'){
+        if(countFirstEvent !== 1){
+            showInDisplay(e);
+        }else {
+            clearDisplay();
+            showInDisplay(e);
         }
-    }else {
-        if(targetClassName === 'key-operator' || targetClassName === 'key-number'){
-            if(e.target.textContent === 'ce'){
-                clearDisplay();
-            }else if(e.target.textContent === '='){
-                calcExpresion().slice(0, -1);
-            }
-            //Cambiando dinamicamente la información del la pantalla
-            displayCalc.textContent += e.target.textContent;
-        }
-    }
+    };
 });
+
+//Funcion para verificar si va teclando correctamente una expresion numerica.
+const checkoutIn = ()=>{
+    //validar que la expresion cumpla con ciertos requisitos para ser operado.
+    const regex = /[0-9]+\s*[+\-*/]\s*[0-9]+/;
+    console.log(regex.test());
+}
 
 //Funcion para limpiar la pantalla
 const clearDisplay = ()=>{
@@ -44,8 +31,17 @@ const clearDisplay = ()=>{
 }
 
 //Funcion para escribir en pantalla
-const writeInDisplay = ()=>{
-
+const showInDisplay = (e)=>{
+    switch(e.target.textContent){
+        case '=':
+            calcExpresion();
+        break;
+        case 'ce':
+            clearDisplay();
+        break;
+        default :
+            displayCalc.textContent += e.target.textContent;;
+    }
 }
 
 //Funcion para calcular la expresion
